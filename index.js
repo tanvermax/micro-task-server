70d86e8bb9e3debb5646636fc5a94ef1e6bac922
tanver909 <tanveermahidi@gmial.com>
2025-01-19 14:12:27 +0600

    admijn created

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const cookieParser= require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.toqnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,30 +29,51 @@ async function run() {
     // await client.db("admin").command({ ping: 1 });
 
     const userCollection = client.db('earnly').collection("users");
+    // admin api
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedData = {
+        $set: {
+          role: "admin"
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedData);
+      res.send(result);
 
+    })
+  //   app.patch('/users/admin/:id', async (req, res) => {
+  //     const id = req.params.id;
+  //     const filter = { _id: new ObjectId(id) };
+  //     const updatedData = { $set: { role: 'admin' } };
+  //     const result = await userCollection.updateOne(filter, updatedData);
+  //     res.send(result);
+  // });
+  
     // jwt token
-    app.post('/', (req, res)=>{
-      try{
+    app.post('/', (req, res) => {
+      try {
         const user = req.body;
-      const token = jwt.sign(user, process.env.JWT_SECRET,{expiresIn:'5h'})
-      res.cookie('access-token', token);
-      res.json({message :'token generated successfully', token})
-      }catch(error){
-        console.error('error genereted JWT:' ,error);
-        res.status(500).json({error :'INternal server Error'})
-        
+        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '5h' })
+        res.cookie('access-token', token);
+        res.json({ message: 'token generated successfully', token })
+      } catch (error) {
+        console.error('error genereted JWT:', error);
+        res.status(500).json({ error: 'INternal server Error' })
+
       }
 
     })
+
     // get user api
 
     app.get('/users', async (req, res) => {
       const email = req.query.email;
-    
+
       // If email is provided, find the specific user by email
       if (email) {
         const user = await userCollection.findOne({ email: email });
-        
+
         if (user) {
           res.send(user); // Send back the user data
         } else {
@@ -67,8 +88,14 @@ async function run() {
     });
     // new useer
     app.post("/users", async (req, res) => {
+
       try {
-        const newuser = req.body; // Get user data from the request body
+        const newuser = req.body;
+        const query = { email: user.email }
+        const existinguser = await userCollection.findOne(query)
+        if (existinguser) {
+          return res.send({ message: 'user already axists', insertedId: null })
+        } // Get user data from the request body
         const result = await userCollection.insertOne(newuser); // Insert user into the database
         res.send(result); // Send the result back to the client
       } catch (error) {
